cc5282ba40255a14952c32ab4269f97ec1c44702
Cezar Leigos <cezarleigos28@outlook.com>
2021-05-17 19:20:36 -0300

    v0.02 -  Creating Controls
    This time, we started creating our app.php file. This file will control the operations with the database, validating accesses and operations. We also started to implement the css style files to adapt the user panel. Another thing that was added was a folder to store the operational modules that will contain the resources that we are going to use in our system.

---
+++ app.php
@@ -0,0 +1,33 @@
+<?php
+
+   @$operation = $_POST['op'];
+   
+   if($operation == "login"){
+
+   }elseif($operation == "quit"){
+
+   }elseif($operation == "user_create"){
+
+   }elseif($operation == "user_edite"){
+
+   }elseif($operation == "user_delete"){
+
+   }elseif($operation == "group_create"){
+
+   }elseif($operation == "group_edite"){
+
+   }elseif($operation == "group_delete"){
+
+   }elseif($operation == "create_card"){
+
+   }elseif($operation == "create_edite"){
+
+   }elseif($operation == "create_delete"){
+
+   }elseif($operation == "install"){
+
+   }else{
+       header("Location: ./");
+   }
+
+?>
---
+++ app/system/php/modules/themes.php
@@ -0,0 +1,25 @@
+<?php
+
+   $theme_color = "basic";
+
+   if($theme_color == "theme_basic"){
+
+   }elseif($theme_color == "theme_red"){
+
+   }elseif($theme_color == "theme_blue"){
+   	
+   }elseif($theme_color == "theme_yellow"){
+   	
+   }elseif($theme_color == "theme_green"){
+   	
+   }elseif($theme_color == "theme_red"){
+   	
+   }elseif($theme_color == "theme_purple"){
+   	
+   }elseif($theme_color == "theme_black"){
+   	
+   }elseif($theme_color == "theme_noturnal"){
+   	
+   }
+
+?>
